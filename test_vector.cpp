a76de1069d0363c533e38cb1d7facc806bc781da
rkuznetc <91145397+rkuznetc@users.noreply.github.com>
2022-07-11 23:09:02 +0300

    Update test_vector.cpp

--- test_vector.cpp
+++ test_vector.cpp
@@ -8,14 +8,14 @@
 template <typename Type, int N>
 Type arr_length(const std::array<Type, N>& arr){
     Type temp = arr[0] * arr[0];
-    for(const auto& elem : arr) { temp += elem * elem; }
+    for(auto i = 1; i<N; i++) { temp += arr[i] * arr[i]; }
     return temp;
 }
 
 template <typename Type>
 Type vec_length(const std::vector<Type>& vec){
     Type temp = vec[0] * vec[0];
-    for(const auto& elem : vec) { temp += elem * elem; }
+    for(auto i = 1; i<vec.size(); i++) { temp += vec[i] * vec[i]; }
     return temp;
 }
 
@@ -32,89 +32,59 @@ class Vector {
     std::array<Type, N> data_;
 
 public:
-    constexpr Vector() {
-        data_.fill(0); 
-    }
+    constexpr Vector() = default;
     constexpr Vector(const Vector<Type, N>& other) {
         data_ = other.data_;
     }
-    constexpr Vector(std::initializer_list<Type> list) {
+    constexpr Vector(const std::initializer_list<Type>& list) {
         std::copy(list.begin(), list.end(), data_.begin());
     }
     
     const Type& operator[](unsigned int i) const { return data_[i]; }
     Type& operator[](unsigned int i) { return data_[i]; }
 
-    Vector<Type, N> operator*=(Type num) {
+    Vector<Type, N>& operator*=(Type num) {
         for(auto& elem : data_) {
             elem *= num;
         }
         return *this;
     }
-    Vector<Type, N> operator/=(Type num) {
+    Vector<Type, N>& operator/=(Type num) {
         for(auto& elem : data_) {
             elem /= num;
         }
         return *this;
     }
-    Vector<Type, N> operator+=(const Vector<Type, N>& other) {
-        for(auto i = 0; i<N; i++) { data_[i] += other[i]; }
+    Vector<Type, N>& operator+=(const Vector<Type, N>& other) {
+        for(int i = 0; i<N; i++) { data_[i] += other[i]; }
         return *this;
     }
-    Vector<Type, N> operator-=(const Vector<Type, N>& other) {
-        for(auto i = 0; i<N; i++) { data_[i] -= other[i]; }
+    Vector<Type, N>& operator-=(const Vector<Type, N>& other) {
+        for(int i = 0; i<N; i++) { data_[i] -= other[i]; }
         return *this;
     }
 
     Vector<Type, N> operator*(Type num) const {
-        Vector<Type, N> temp;
-        for(auto i = 0; i<N; i++) { temp[i] = data_[i] * num; }
-        return temp;
+        return Vector<Type, N>(*this) *= num;
     }
 
     Vector<Type, N> operator/(Type num) const {
-        Vector<Type, N> temp;
-        for(auto i = 0; i<N; i++) { temp[i] = data_[i] / num; }
-        return temp;
+        return Vector<Type, N>(*this) /= num;
     }
 
     Vector<Type, N> operator+(const Vector<Type, N>& other) const {
-        Vector<Type, N> temp;
-        for(auto i = 0; i<N; i++) { temp[i] = data_[i] + other[i]; }
-            return temp;
+        return Vector<Type, N>(*this) += other;
     }
     Vector<Type, N> operator-(const Vector<Type, N>& other) const {
-        Vector<Type, N> temp;
-        for(auto i = 0; i<N; i++) { temp[i] = data_[i] - other[i]; }
-        return temp;
-    }
-
-    bool operator<(const Vector<Type, N>& other) const {
-        return arr_length<Type, N>(data_) < arr_length<Type, N>(other.data_);
-    }
-
-    bool operator>(const Vector<Type,N>& other) const {
-        return arr_length<Type, N>(data_) > arr_length<Type, N>(other.data_);
+        return Vector<Type, N>(*this) -= other;
     }
 
     bool operator==(const Vector<Type, N>& other) const {
-        return (operator<(other) == operator>(other));
+        return (arr_length<Type, N>(data_) == arr_length<Type, N>(other.data_));
     }
 
-    friend Vector<Type, N> operator*(Type num, const Vector<Type, N>& vec);
-    friend Vector<Type, N> operator/(Type num, const Vector<Type, N>& vec);
     friend std::ostream& operator<< <>(std::ostream& out,  const Vector& vec);
 };
-
-template <typename Type, int N>
-Vector<Type, N> operator*(Type num, const Vector<Type, N>& vec) {
-    return vec * num;
-}
-
-template <typename Type, int N>
-Vector<Type, N> operator/(Type num, const Vector<Type, N>& vec) {
-    return vec / num;
-}
 
 template <typename Type, int N>
 std::ostream& operator<<(std::ostream& out, const Vector<Type, N>& vec) {
@@ -143,7 +113,7 @@ public:
         size_ = data_.size(); 
         data_ = other.data_;
     }
-    constexpr Vector(std::initializer_list<Type> list) {
+    constexpr Vector(const std::initializer_list<Type>& list) {
         Resize(list.size());
         std::copy(list.begin(), list.end(), data_.begin());
     }
@@ -160,80 +130,46 @@ public:
     Type& operator[](unsigned int i) { return data_[i]; }
 
 
-    Vector<Type, Dynamic> operator*=(Type num) {
+    Vector<Type, Dynamic>& operator*=(Type num) {
         for(auto& elem : data_) { elem *= num; }
         return *this;
     }
-    Vector<Type, Dynamic> operator/=(Type num) {
+    Vector<Type, Dynamic>& operator/=(Type num) {
         for(auto& elem : data_) { elem /= num; }
         return *this;
     }
     Vector<Type, Dynamic>& operator+=(const Vector<Type, Dynamic>& other) {
-        // Resize(other.size_);
         for(auto i = 0; i<size_; i++) { data_[i] += other[i]; }
         return *this;
     }
 
     Vector<Type, Dynamic>& operator-=(const Vector<Type, Dynamic>& other) {
-        // Resize(other.size_);
         for(auto i = 0; i<size_; i++) { data_[i] -= other[i]; }
         return *this;
     }
 
     Vector<Type, Dynamic> operator*(Type num) const {
-        Vector<Type, Dynamic> temp;
-        temp.Resize(size_);
-        for(auto i = 0; i<size_; i++) { temp[i] = data_[i] * num; }
-        return temp;
+        return Vector<Type, Dynamic>(*this) *= num;
     }
 
     Vector<Type, Dynamic> operator/(Type num) const {
-        Vector<Type, Dynamic> temp;
-        temp.Resize(size_);
-        for(auto i = 0; i<size_; i++) { temp[i] = data_[i] / num; }
-        return temp;
+        return Vector<Type, Dynamic>(*this) /= num;
     }
 
     Vector<Type, Dynamic> operator+(const Vector<Type, Dynamic>& other) const {
-        Vector<Type, Dynamic> temp;
-        temp.Resize(size_);
-        for(auto i = 0; i<size_; i++) { temp[i] = data_[i] + other[i]; }
-        return temp;
+        return Vector<Type, Dynamic>(*this) += other;
     }
 
     Vector<Type, Dynamic> operator-(const Vector<Type, Dynamic>& other) const {
-        Vector<Type, Dynamic> temp;
-        temp.Resize(size_);
-        for(auto i = 0; i<size_; i++) { temp[i] = data_[i] - other[i]; }
-        return temp;
-    }
-
-    bool operator<(const Vector<Type, Dynamic>& other) const {
-        return vec_length<Type>(data_) < vec_length<Type>(other.data_);
-    }
-
-    bool operator>(const Vector<Type, Dynamic>& other) const {
-        return vec_length<Type>(data_) > vec_length<Type>(other.data_);
+        return Vector<Type, Dynamic>(*this) -= other;
     }
 
     bool operator==(const Vector<Type, Dynamic>& other) const {
-        return (operator<(other) == operator>(other));
+        return (vec_length<Type>(data_) == vec_length<Type>(other.data_));
     }
 
-    friend Vector<Type, Dynamic> operator*(Type num, const Vector<Type, Dynamic>& vec);
-    friend Vector<Type, Dynamic> operator/(Type num, const Vector<Type, Dynamic>& vec);
     friend std::ostream& operator<< <>(std::ostream& out,  const Vector<Type, Dynamic>& vec);
 };
-
-template <typename Type>
-Vector<Type, Dynamic> operator*(Type num, const Vector<Type, Dynamic>& vec) {
-    return vec * num;
-}
-
-template <typename Type>
-Vector<Type, Dynamic> operator/(Type num, const Vector<Type, Dynamic>& vec) {
-    return vec / num;
-}
 
 template <typename Type>
 std::ostream& operator<<(std::ostream& out, const Vector<Type,Dynamic>& vec) {
@@ -241,11 +177,22 @@ std::ostream& operator<<(std::ostream& out, const Vector<Type,Dynamic>& vec) {
     return out << "\n";
 }
 
+
 int main() {
-    std::initializer_list<int> list1 {6, 7, 8};
-    std::initializer_list<int> list2 {1, 2, 3};
+    std::initializer_list<int> list1 {3, 4, 5};
+    std::initializer_list<int> list2 {5, 4, 3};
 
     Vector<int, -1> example(list1);
     Vector<int, -1> example2(list2);
-    std::cout <<(example > example2) << (example < example2) << (example == example2);
+    Vector<int, -1> test(example2);
+    example += example2;
+    std::cout << example << example2 << test;
+    std::cout << (example == example2) << "\n";
+
+    Vector<int, -1> example3;
+    example3 = example2 * 5;
+    std::cout << example3;
+    Vector<int, -1> example4;
+    example4 = example3 + example2;
+    std::cout << example4;
 }
